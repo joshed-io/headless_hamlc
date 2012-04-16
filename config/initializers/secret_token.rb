c105c1e95d6f43506dd2af4be6d177a737d078e2
Josh Dzielak <jdzielak@gmail.com>
2012-04-15 22:52:21 -0700

    Result of 'rails new headless_hamlc'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeadlessHamlc::Application.config.secret_token = 'fba758b63ca3ee9fde26f09e41c7ff1211a12664e7befb5d5cb59f25eca2f7ca6bc85972bf52658bc638dc8b99484f19402147b5dae6767fee9bedb4d215c485'
